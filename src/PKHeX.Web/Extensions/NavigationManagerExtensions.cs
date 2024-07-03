91979de3031958eab02930515a0a6d2a722f02ce
Arley Padua <Arley Padua>
2024-07-03 15:58:30 +0200

    feat: proper naviagion history on pagination

--- src/PKHeX.Web/Extensions/NavigationManagerExtensions.cs
+++ src/PKHeX.Web/Extensions/NavigationManagerExtensions.cs
@@ -1,3 +1,4 @@
+using System.Web;
 using Microsoft.AspNetCore.Components;
 using PKHeX.Facade.Pokemons;
 using PKHeX.Web.Model;
@@ -6,9 +7,15 @@ namespace PKHeX.Web.Extensions;
 
 public static class NavigationManagerExtensions
 {
-    public static string CurrentRoute(this NavigationManager navigation) => navigation.Uri.Replace(navigation.BaseUri, string.Empty);
+    public static string CurrentRoute(this NavigationManager navigation) =>
+        navigation.Uri.Replace(navigation.BaseUri, string.Empty);
 
-    public static void NavigateToPokemon(this NavigationManager navigation, PokemonSource source, UniqueId uniqueId, bool replace = false) =>
+    public static void NavigateToPokemon(this NavigationManager navigation, PokemonSource source, UniqueId uniqueId,
+        bool replace = false) =>
         navigation.NavigateTo($"/pokemon/{source.RouteString()}/{uniqueId}", replace: replace);
 
+    public static void StoreOnQuery(this NavigationManager navigation, Dictionary<string, object?> parameters)
+    {
+        navigation.NavigateTo(navigation.GetUriWithQueryParameters(parameters));
+    }
 }
