017727ee3cbce65e6035c61f5254ba197b3ea342
Arley Padua <Arley Padua>
2024-08-07 13:24:28 +0200

    refactor: remove commented code

--- src/PKHeX.Facade/Repositories/ItemRepository.cs
+++ src/PKHeX.Facade/Repositories/ItemRepository.cs
@@ -36,9 +36,3 @@ public record ItemDefinition(ushort Id, string Name)
 
     public bool IsNone => Id == None;
 }
-
-// public static class ItemRepositoryExtensions
-// {
-//     public static ItemDefinition GetItem(this ItemRepository repository, int id) =>
-//         repository.GetItem(Convert.ToUInt16(id));
-// }
