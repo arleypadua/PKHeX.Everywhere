8771bf15cd70e28bcaa067675c2daf9cd8ea124f
Arley Pádua <Arley Pdua>
2024-11-29 20:59:20 +0100

    fix: unknown gender

--- src/PKHeX.Facade/Gender.cs
+++ src/PKHeX.Facade/Gender.cs
@@ -16,12 +16,13 @@ public sealed class Gender : Enumeration
     public static readonly Gender Male = new (0, "Male", "♂");
     public static readonly Gender Female = new (1, "Female", "♀");
     public static readonly Gender Genderless = new(2, "Genderless", "⚲");
+    public static readonly Gender Unknown = new (int.MaxValue, "Unknown", "⁇");
 
     public static Gender FromByte(byte value) => value switch
     {
         0 => Male,
         1 => Female,
         2 => Genderless,
-        _ => throw new ArgumentOutOfRangeException(nameof(value), $"Value {value} not supported")
+        _ => Unknown
     };
 }
