63ba61caf89ab7ce211d805b15b46530042b8f28
Arley Pádua <Arley Pdua>
2025-05-02 22:43:05 +0200

    feat: adds seo

--- src/PKHeX.Web/_blog/_data/site.js
+++ src/PKHeX.Web/_blog/_data/site.js
@@ -2,10 +2,12 @@ export default function () {
     const title = 'PKHeX.Web - Blog'
     const isLocal = ['serve', 'watch'].includes(process.env.ELEVENTY_RUN_MODE)
     const baseUrl = isLocal ? '/' : '/blog/'
+    const url = "https://pkhex-web.github.io/blog"
     
     return {
         title,
         isLocal,
         baseUrl,
+        url
     };
 }
